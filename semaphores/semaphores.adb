5d07d4123c165ab74a20dd1a4b19cfb9b66f608f
xyderos <kosxyderos@gmail.com>
2020-11-02 15:23:20 +0100

    added concurrency in ada

---
+++ semaphores/semaphores.adb
@@ -0,0 +1,24 @@
+-- Package: Semaphores
+--
+-- ==> Complete the code at the indicated places
+
+package body Semaphores is
+   protected body CountingSemaphore is
+
+      entry Wait when Count > 0 is
+	      begin
+	      --decrement
+	      Count := Count - 1;
+	
+	   end Wait;
+      
+      entry Signal when Count < MaxCount is
+	      begin
+	      --increment
+	      Count := Count + 1;
+	   
+      end Signal;
+
+   end CountingSemaphore;
+end Semaphores;
+
